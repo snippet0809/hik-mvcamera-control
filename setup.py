2010857beaafe2d70978e906a73aab302f0f0975
SpetJ <snippet0809@163.com>
2025-11-11 17:55:26 +0800

    feat(package): 添加pyproject.toml并移除setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="hik-camera-control",
-    version="0.0.1",
-    author="https://github.com/snippet0809",
-    python_requires=">=3.11",
-    packages=find_packages(),
-)
