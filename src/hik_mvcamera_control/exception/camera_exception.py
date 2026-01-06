a2dc00e3290e3135dcf1dafcc4f010f213fccb4f
lijian <34831075+lijian0711@users.noreply.github.com>
2026-01-06 17:15:53 +0800

    modify
    Signed-off-by: lijian <34831075+lijian0711@users.noreply.github.com>

--- src/hik_mvcamera_control/exception/camera_exception.py
+++
@@ -1,5 +0,0 @@
-class CameraError(Exception):
-    def __init__(self, err_msg: str, err_code: str | None = None):
-        self.err_msg = err_msg
-        self.err_code = err_code
-        super().__init__(self.err_msg)
--- src/hik_mvcamera_control/logger/logger.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-log = logging.getLogger("HIK-MVCAMERA-CONTROL")
