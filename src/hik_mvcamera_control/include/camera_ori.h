dc379e611384e22fae439783d70a653a34b00a19
SpetJ <snippet0809@163.com>
2026-03-28 13:33:52 +0800

    chore:初始化c++项目

--- src/hik_mvcamera_control/include/camera_ori.h
+++
@@ -1,9 +0,0 @@
-#include "CameraParams.h"
-#include "MvCameraControl.h"
-#include "MvErrorDefine.h"
-#include "MvISPErrorDefine.h"
-#include "MvObsoleteInterfaces.h"
-#include "ObsoleteCamParams.h"
-#include "PixelType.h"
-
-#include "camera_control.h"
